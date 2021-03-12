41a6ddebd7367046acd63a6090d1ab7e020b3e2f
David Rodriguez <david@bsdadm.com>
2021-03-12 18:45:55 -0500

    update transactions.js to reflect new es6

--- models/transaction.js
+++ models/transaction.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
 const Schema = mongoose.Schema;
 
@@ -20,4 +20,4 @@ const transactionSchema = new Schema({
 
 const Transaction = mongoose.model("Transaction", transactionSchema);
 
-module.exports = Transaction;
+export default Transaction;
